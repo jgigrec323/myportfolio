89d60c983e51f93a915da6a35085948e595a344d
Jean Galant Yombouno <jeangalanty@gmail.com>
2024-04-08 15:18:55 +0300

    going, going

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,15 +1,77 @@
-import React from 'react'
-
+"use client"
+import { useGSAP } from '@gsap/react';
+import React, { useRef } from 'react';
+import gsap from "gsap";
+import SplitType from 'split-type';
 function Hero() {
+    const text1 = useRef(null);
+    const text2 = useRef(null);
+    const btn = useRef(null);
+    const mainText = useRef(null);
+
+    useGSAP(() => {
+        //here simulating the entrance animation
+        mainText.current.style.opacity = 1
+        btn.current.style.opacity = 1
+        if (mainText.current) {
+            const text = new SplitType(mainText.current, { types: 'words,chars' })
+
+            gsap.from(text.chars, {
+                yPercent: 100,
+                rotate: "20deg",
+                scaleY: 0.5,
+                duration: 0.4,
+                stagger: 0.05
+            })
+            gsap.from(btn.current, {
+                opacity: 0,
+                delay: 2.9
+
+            })
+
+        }
+
+
+        const tl = gsap.timeline({ paused: true });
+
+        tl.to(text1.current, { yPercent: -103, duration: 0.5, ease: "power2.inOut" })
+            .to(text2.current, { yPercent: -120, duration: 0.5, ease: "power2.inOut" }, "-=0.4");
+
+        btn.current.addEventListener("mouseenter", () => {
+            tl.play();
+        });
+
+        btn.current.addEventListener("mouseleave", () => {
+            tl.reverse();
+        });
+
+        return () => {
+            btn.current.removeEventListener("mouseenter", () => {
+                tl.play();
+            });
+
+            btn.current.removeEventListener("mouseleave", () => {
+                tl.reverse();
+            });
+        };
+    }, []);
+
     return (
         <div className='hero'>
-            <p className='mainText'>Hey ! I'm <span className='myname'>Jean Galant .</span><br />
-                Full stack <span className='myc'>Web & Mobile</span><br />
-                Developer</p>
-            <button className='btn'>Got a project ?</button>
-            <div className="scrollIndicator">Scroll<br />Down</div>
+            <div className='mainText' ref={mainText}>
+                <div>Hey ! I'm <span className='myname'>Jean Galant .</span></div>
+                <div>Full stack <span className='myc'>Web & Mobile</span></div>
+                <div>Developer</div>
+            </div>
+            <button className='btn' ref={btn}>
+                <div>
+                    <span className='text1' ref={text1}>Got a project ?</span>
+                    <span className='text2' ref={text2}>Let's talk</span>
+                </div>
+            </button>
+            <div className="scrollIndicator down">Scroll<br />Down</div>
         </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
--- src/app/components/Menu.jsx
+++ src/app/components/Menu.jsx
@@ -6,7 +6,7 @@ function Menu() {
             <div className="top">
                 <div className="logo">
                     <svg id="Composant_1_2" data-name="Composant 1 – 2" xmlns="http://www.w3.org/2000/svg" width="99" height="96" viewBox="0 0 99 96">
-                        <text id="JG." transform="translate(51 77)" fill="#a1baff" font-size="80" font-family="FuturaBT-ExtraBlackCondItalic, Futura XBlkCnIt BT" font-weight="900" font-style="italic"><tspan x="-47.09" y="0">JG.</tspan></text>
+                        <text id="JG." transform="translate(51 77)" fill="#a1baff" fontSize="80" fontFamily="FuturaBT-ExtraBlackCondItalic, Futura XBlkCnIt BT" fontWeight="900" fontStyle="italic"><tspan x="-47.09" y="0">JG.</tspan></text>
                     </svg>
                 </div>
                 <div className="closeBtn">
@@ -22,7 +22,7 @@ function Menu() {
             </ul>
             <div className="bottom">
                 <a href="#" className="active">En</a>
-                <a href="#" className="active">Fr</a>
+                <a href="#" className="">Fr</a>
             </div>
         </div>
     )
